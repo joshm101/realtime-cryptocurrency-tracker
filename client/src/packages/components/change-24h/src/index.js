e2248341f2f47787e3cbdd82810593fbb3571075
Joshua Mora <jshm101@gmail.com>
2018-04-04 18:15:38 -0700

    added reusable component to display change 24h amount

---
+++ client/src/packages/components/change-24h/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import NumberWithCommas from 'number-with-commas';
+
+const Change24h = ({
+  symbol,
+  changeAmount,
+  changePercent,
+}) => {
+  let changeColor;
+  if (change24h < 0) {
+    changeColor = '#D50000';
+  }
+  if (change24h > 0) {
+    changeColor = '#1B5E20';
+  }
+  if (change24h === 0) {
+    changeColor = '#000';
+  }
+  const styles = {
+    changeStyle: {
+      color: changeColor,
+    },
+  };  
+  return (
+    <div>
+      <span>{symbol}</span>
+      <span>
+        &nbsp;<NumberWithCommas value={changeAmount.toFixed(6)} />
+      </span>
+      <span>
+        &nbsp;(
+        <span style={styles.changeStyle}>
+          {changePercent > 0 ? '+' : ''}{changePercent.toFixed(2)}%
+        </span>
+        )
+      </span>
+    </div>
+  );
+}
+
+export default Change24h;
