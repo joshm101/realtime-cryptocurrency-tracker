cff9d6766680f308195e10acf350ebb079774da0
Joshua Mora <jshm101@gmail.com>
2018-05-13 20:32:09 -0700

    remove favicon reference

--- server/app.js
+++ server/app.js
@@ -14,7 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.resolve(__dirname + '../../client/build/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
