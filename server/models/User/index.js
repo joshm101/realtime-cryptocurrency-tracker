a60f7e5a5351d9f1d1e0db02d7832f55e0c93a66
Joshua Mora <jshm101@gmail.com>
2018-05-14 00:56:44 -0700

    don't include email in generated JWT

--- server/models/User/index.js
+++ server/models/User/index.js
@@ -56,7 +56,6 @@ schema.methods.generateJwt = () => {
   const expiry = moment().add(3, 'hours').toDate()
   return jwt.sign({
     _id: this._id,
-    email: this.email,
     exp: parseInt(expiry.getTime() / 1000)
   }, RCT_SECRET)
 }
